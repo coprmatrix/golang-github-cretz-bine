758e3b5bc056d17cee0202981337af85912a65e4
u <u>
2025-04-08 00:43:55 +0300

    update

--- regex.pl
+++
@@ -1,6 +0,0 @@
-s~%global\s+goipath\s+(.*)/(.*)~%global goihead \2\n%global goipath \1/%{goihead}\n~g;
-s~(Version:)\s*([^0-9\.]*)([0-9\.]*)([^\s]*)\s*~\1  \3\n%define oldver \2\3\4\nBuildRequires: coreutils\n~g;
-s~(^%gometa.*)~%{?!tag:%{?!commit:%global tag v%{oldver}}}\n\1\n~g;
-s~(%build.*)~\1\n%{prebuild}\n~g;
-s~%gocheck~~g;
-s~Source:.*~%define stag %{?tag}%{?!tag:%commit}\nSource0: https://%{goipath}/archive/%{stag}/%{goihead}-%{stag}.tar.gz~g;
